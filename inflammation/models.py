8709cf6e59d7868d32cc557069c5f2cba4556e31
jesszcarr <jesszcarr@gmail.com>
2025-10-21 22:54:53 +0100

    hi

---
+++ inflammation/models.py
@@ -0,0 +1,61 @@
+"""Module containing models representing patients and their data.
+
+The Model layer is responsible for the 'business logic' part of the software.
+
+Patients' data is held in an inflammation table (2D array) where each row contains
+inflammation data for a single patient taken over a number of days
+and each column represents a single day across all patients.
+"""
+
+import numpy as np
+
+
+def load_csv(filename):
+    """Load a Numpy array from a CSV
+
+    :param filename: Filename of CSV to load
+    """
+    return np.loadtxt(fname=filename, delimiter=",")
+
+
+def daily_mean(data):
+    """Calculate the daily mean of a 2d inflammation data array."""
+    return np.mean(data, axis=0)
+
+
+def daily_max(data):
+    """Calculate the daily max of a 2d inflammation data array."""
+    return np.max(data, axis=0)
+
+
+def daily_min(data):
+    """Calculate the daily min of a 2d inflammation data array."""
+    return np.min(data, axis=0)
+
+
+import numpy as np
+import pytest
+
+
+def patient_normalise(data):
+    error
+    """
+    Normalise patient data between 0 and 1 of a 2D inflammation data array.
+
+    Any NaN values are ignored, and normalised to 0
+
+    :param data: 2D array of inflammation data
+    :type data: ndarray
+
+    """
+    if not isinstance(data, np.ndarray):
+        raise TypeError("data input should be ndarray")
+    if len(data.shape) != 2:
+        raise ValueError("inflammation array should be 2-dimensional")
+    if np.any(data < 0):
+        raise ValueError("inflammation values should be non-negative")
+    max = np.nanmax(data, axis=1)
+    with np.errstate(invalid="ignore", divide="ignore"):
+        normalised = data / max[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    return normalised
